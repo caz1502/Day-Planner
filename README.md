3d3345f29aeabd7df176e6114113c1c9e8b65591
Carol Shiers <carol.shiers@gmail.com>
2021-10-11 09:18:28 +1000

    updating all files

---
+++ README.md
@@ -0,0 +1,40 @@
+## Daily Schduler
+
+Live Application Link 👀 **https://caz1502.github.io/Day-Planner/ **
+
+## Your Task
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## User Story
+
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Website Image
+![Code Quiz](./assets/images/codequiz.png)
+
+## Authors, Acknowledgement, & Resources 🤝
+Teaching Crew at UniSA Coding Bootcamp 🎉
+W3Schools 🤓
+GitHub 
+Youtube
+Google
+StackOverflow
--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function displayTime() {
   }
   setInterval(displayTime, 1000);
 
-
-// this fuction defines the current time
- 
+// this fuction defines the current time 
 function currentTime() {
   var current = moment().hours()
   var time = $('.time-block');
@@ -20,32 +18,27 @@ function currentTime() {
 
 // this is the if statement that changes current hour
     if (hour === current) {
-      $(this).children('.col-sm-9').attr('class', 'present col-sm-9 description')
+      $(this).children('.col-9').attr('class', 'present col-9 description')
   // this is the if statement that changes past hour hour
     } else if (current > hour) {
-      $(this).children('.col-sm-9').attr('class', 'past col-sm-9 description')
+      $(this).children('.col-9').attr('class', 'past col-9 description')
   //  this is the if statement if the previous 2 statement are not met then this is the colour
     } else {
-      $(this).children('.col-sm-9').attr('class', 'future col-sm-9 description')
+      $(this).children('.col-9').attr('class', 'future col-9 description')
   
     }
   })
 }
 currentTime()
 
- //Save to local storage
-
+ //Save to local storage - replace removes double quotes
  button.on('click', function (event) {
   event.preventDefault();
-  var text = $(this).siblings('.col-sm-9').val();
+  var text = $(this).siblings('.col-9').val().replace();
   var parent = $(this).parent().attr('id');
   localStorage.setItem(parent, JSON.stringify(text));
 })
 
-// prints local storage on screen
-
-
-  // Get item from local storage if any
 
 $('#9 textarea').val(localStorage.getItem('9'));
 $('#10 textarea').val(localStorage.getItem('10'));
@@ -58,4 +51,10 @@ $('#16 textarea').val(localStorage.getItem('16'));
 $('#17 textarea').val(localStorage.getItem('17'));
 
 
+function deleteItems() {
+window.localStorage.clear();
+window.location.reload();
+}
+
+ 
   
